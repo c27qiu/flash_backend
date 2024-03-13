aea799a122a5391319bc5edc671dec7feeb9ebc7
Cathy Qiu <cathyqiu0@gmail.com>
2024-03-13 00:30:18 -0400

    broadcast url string

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(data)
             
             # s3_url = json.loads(data)
             # wall_name = s3_url["wall_name"]
